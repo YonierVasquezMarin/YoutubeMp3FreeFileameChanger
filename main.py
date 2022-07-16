3bd0a05ffe9e6e718843e135a77623729951e9ee
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-16 18:56:38 -0500

    Algunos pasos agregados

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
-PASOS
-1. Abrir el explorador de archivos y seleccionar la carpeta de los audios
-2. Verificar que se haya seleccionado una carpeta
-3. Obtener el nombre de los archivos y crear un objeto por cada archivo
+CICLO DE VIDA DEL PROGRAMA
+- Abrir el explorador de archivos y seleccionar la carpeta de los audios
+- Verificar que se haya seleccionado una carpeta
+- Obtener el nombre de los archivos, crear un objeto por cada archivo y agregarlo a una lista
+- Por cada archivo de la lista ejecutar el metodo encargado de cambiar el nombre del archivo
 '''
