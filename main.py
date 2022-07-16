35743080667228fefc91e32b8f2a862607835e59
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-16 18:40:13 -0500

    Algunos pasos creados

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+PASOS
+1. Abrir el explorador de archivos y seleccionar la carpeta de los audios
+2. Verificar que se haya seleccionado una carpeta
+3. Obtener el nombre de los archivos y crear un objeto por cada archivo
+'''
